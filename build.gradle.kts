82efcd7d2699973c51330307faa96781e367cf3c
Tursonkul <telvintamer@gmail.com>
2024-03-25 17:49:28 +0500

    add migration, and change java version to 17

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ group = "com.alabar"
 version = "0.0.1-SNAPSHOT"
 
 java {
-	sourceCompatibility = JavaVersion.VERSION_21
+	sourceCompatibility = JavaVersion.VERSION_17
 }
 
 configurations {
@@ -22,6 +22,7 @@ repositories {
 }
 
 dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.flywaydb:flyway-core")
